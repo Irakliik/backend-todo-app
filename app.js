4d0b917ae924e428ad39017c0d37ee49a9a93f08
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-09-15 22:44:37 +0400

    added some endpoints

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import bodyParser from "body-parser";
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { db, initDB } from "./util/database.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,8 +15,37 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("index", { nana: "lamazo" });
+app.get("/", async (req, res) => {
+  const [rows] = await db.execute("SELECT * FROM todos");
+  const todos = rows.map((row) => ({
+    ...row,
+    completed: !!row.completed,
+  }));
+
+  res.render("index", { todos: todos });
 });
 
-app.listen(3000);
+app.post("/add-todo", async (req, res) => {
+  const newTodo = req.body.todo;
+  if (newTodo) {
+    await db.execute("INSERT INTO todos (todo) VALUES (?)", [newTodo]);
+  }
+  res.redirect("/");
+});
+
+app.post("/delete-todo/:id", async (req, res) => {
+  const id = req.params.id;
+  const response = await db.execute("DELETE FROM todos WHERE id = ?", [id]);
+  console.log(response);
+  res.redirect("/");
+});
+
+initDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server started successfully");
+    });
+  })
+  .catch((err) => {
+    console.log("could not start server", err);
+  });
--- util/database.js
+++ util/database.js
@@ -1,5 +1,7 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
 
+dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -9,4 +11,22 @@ const pool = mysql.createPool({
 
 const db = pool.promise();
 
-export { db };
+async function initDB() {
+  try {
+    // =========================================================================
+
+    await db.query(
+      `CREATE TABLE IF NOT EXISTS todos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    todo TEXT NOT NULL, 
+    completed BOOLEAN DEFAULT FALSE )`
+    );
+
+    console.log("Table ready");
+  } catch (err) {
+    console.error("Error initializing DB:", err);
+    throw err;
+  }
+}
+
+export { db, initDB };
