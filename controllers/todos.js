a557024a692c1ad61d1fb5deb5bd9eef665fba59
Irakliik <159915721+Irakliik@users.noreply.github.com>
2025-09-16 18:04:18 +0400

    added error handling

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,45 +1,69 @@
 import { db } from "../util/database.js";
 
 export const getTodos = async (req, res) => {
-  const [rows] = await db.execute("SELECT * FROM todos");
-  const todos = rows.map((row) => ({
-    ...row,
-    completed: !!row.completed,
-  }));
+  try {
+    const [rows] = await db.execute("SELECT * FROM todos");
+    const todos = rows.map((row) => ({
+      ...row,
+      completed: !!row.completed,
+    }));
 
-  res.render("index", { todos: todos });
+    res.render("index", { todos: todos });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching todos");
+  }
 };
 
 export const addTodo = async (req, res) => {
-  const newTodo = req.body.todo;
-  if (newTodo) {
+  try {
+    const newTodo = req.body.todo;
+
+    if (newTodo.trim() === "") {
+      throw new Error("Todos cannot be empty");
+    }
+
     await db.execute("INSERT INTO todos (todo) VALUES (?)", [newTodo]);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    res.redirect("/");
   }
-  res.redirect("/");
 };
 
 export const deleteTodo = async (req, res) => {
-  const id = req.params.id;
-  const response = await db.execute("DELETE FROM todos WHERE id = ?", [id]);
-  console.log(response);
-  res.redirect("/");
+  try {
+    const id = req.params.id;
+    const response = await db.execute("DELETE FROM todos WHERE id = ?", [id]);
+    console.log(response);
+    res.redirect("/");
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export const updateTodo = async (req, res) => {
-  const id = req.params.id;
-  const [[{ completed }]] = await db.execute(
-    "SELECT completed FROM todos WHERE id = ?",
-    [id]
-  );
+  try {
+    const id = req.params.id;
+    const [[{ completed }]] = await db.execute(
+      "SELECT completed FROM todos WHERE id = ?",
+      [id]
+    );
 
-  if (completed) {
-    await db.execute("UPDATE todos SET completed = ? WHERE id = ?", [
-      false,
-      id,
-    ]);
-  } else {
-    await db.execute("UPDATE todos SET completed = ? WHERE id = ?", [true, id]);
+    if (completed) {
+      await db.execute("UPDATE todos SET completed = ? WHERE id = ?", [
+        false,
+        id,
+      ]);
+    } else {
+      await db.execute("UPDATE todos SET completed = ? WHERE id = ?", [
+        true,
+        id,
+      ]);
+    }
+
+    res.redirect("/");
+  } catch (err) {
+    console.error(err);
   }
-
-  res.redirect("/");
 };
